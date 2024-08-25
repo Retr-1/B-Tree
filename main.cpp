3c9b72fb203a9f6486b8a2c2426b185bad78e1ff
Chuck Norris <me@example.com>
2024-08-25 21:22:15 +0200

    changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <tuple>
 
 template <unsigned KEY_MAX, typename Data>
 class Node {
@@ -6,22 +8,58 @@ class Node {
 	struct Container {
 		Data data;
 		int count = 0;
+
+		Container(Data& data) {
+			this->data = data;
+		}
+		
 	};
 
-	Node* pointers[KEY_MAX];
-	Container values[KEY_MAX];
-	int children = 0;
+	std::vector<Container> items;
+	std::vector<Node> children;
 
 public:
-	void insert(Data& data) {
-		for (int i = 0; i < children; i++) {
-			if (data < values[i].data) {
-				return pointers[i]->insert(data);
-			}
-			else if (data == values[i].data) {
-				values[i].count++;
+	Node() {
+		items.reserve(KEY_MAX);
+		children.reserve(KEY_MAX+1);
+	}
+
+	std::tuple<Container, Node, Node> insert(Data& data) {
+		for (int i = 0; i < items.size(); i++) {
+			if (data == items[i].data) {
+				items[i].data++;
 				return;
 			}
+			else if (data < items[i].data) {
+				if (children.size() == 0) { // is a leaf node
+					items.insert(items.begin() + i, Container(data));
+				}
+				else {
+					return children[i].insert(data);
+				}
+			}
+		}
+
+		if (children.size() == 0) {
+			items.push_back(Container(data));
+
+			if (items.size() > KEY_MAX) {
+				int split = items.size() / 2;
+				//Container splitter = items[items.size() / 2];
+				Node left;
+				for (int i = 0; i < split; i++) {
+					left.items.push_back(items[i]);
+				}
+				Node right;
+				for (int i = split; i < KEY_MAX; i++) {
+					right.items.push_back(items[i]);
+				}
+				return std::make_tuple(items[split], left, right);
+				
+			}
+		}
+		else {
+			children[KEY_MAX + 1].insert(data);
 		}
 	}
 };
