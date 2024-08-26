26023672ab388032410794feb5bc36c7f7329321
Chuck Norris <me@example.com>
2024-08-26 18:05:49 +0200

    sotring pointers

--- main.cpp
+++ main.cpp
@@ -14,29 +14,8 @@ class Node {
 		}
 	};
 
-	std::vector<Container> items;
-	std::vector<Node> children;
-
-	void split_into_parent(Node* parent, int index) {
-		int split_index = items.size() / 2;
-		Node right;
-		for (int i = split_index + 1; i < KEY_MAX; i++) {
-			Container& pop = items.back();
-			right.items.push_back(pop);
-			items.pop_back();
-		}
-
-		if (parent) {
-			parent->items.insert(parent->items.begin() + index, items[split_index]);
-			parent->children.insert(parent->children.begin() + index + 1, right);
-		}
-		else {
-			Node parent_node;
-			parent_node.items.push_back(items[split_index]);
-			parent_node.children.push_back(*this);
-			parent_node.children.push_back(right);
-		}
-	}
+	std::vector<Container*> items;
+	std::vector<Node*> children;
 
 public:
 	Node() {
@@ -44,34 +23,56 @@ public:
 		children.reserve(KEY_MAX+1);
 	}
 
-	void insert(Data& data, Node* parent=nullptr, int index=0) {
+	Node* insert(Data& data, Node* parent=nullptr, int index=0) {
 		for (int i = 0; i < items.size(); i++) {
-			if (data == items[i].data) {
-				items[i].data++;
+			if (data == items[i]->data) {
+				items[i]->data++;
 				return;
 			}
-			else if (data < items[i].data) {
+			else if (data < items[i]->data) {
 				if (children.size() == 0) { // is a leaf node
-					items.insert(items.begin() + i, Container(data));
+					items.insert(items.begin() + i, new Container(data));
 					goto FOR_END;
 				}
 				else {
-					children[i].insert(data);
+					children[i]->insert(data);
 					goto FOR_END;
 				}
 			}
 		}
 
 		if (children.size() == 0) {
-			items.push_back(Container(data));
+			items.push_back(new Container(data));
 		}
 		else {
-			children[KEY_MAX + 1].insert(data);
+			children[KEY_MAX]->insert(data);
 		}
 
 		FOR_END:;
-		if (items.size() > KEY_MAX) {
-			split_into_parent(parent, index);
+
+		if (items.size() <= KEY_MAX) {
+			return parent;
+		}
+
+		int split_index = items.size() / 2;
+		Node* right = new Node();
+		for (int i = split_index + 1; i < KEY_MAX; i++) {
+			Container* pop = items.back();
+			right->items.push_back(pop);
+			items.pop_back();
+		}
+
+		if (parent) {
+			parent->items.insert(parent->items.begin() + index, items[split_index]);
+			parent->children.insert(parent->children.begin() + index + 1, right);
+			return parent;
+		}
+		else {
+			Node* parent_node = new Node();
+			parent_node->items.push_back(items[split_index]);
+			parent_node->children.push_back(this);
+			parent_node->children.push_back(right);
+			return parent_node;
 		}
 	}
 };
