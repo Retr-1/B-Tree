c2a75b5f882f62aa20115074a3e2c0185ce30d0b
Chuck Norris <me@example.com>
2024-09-09 09:47:13 +0200

    some testing

--- main.cpp
+++ main.cpp
@@ -257,6 +257,8 @@ int main() {
 	BTree<4, int> btree;
 
 	btree.insert(10);
+	btree.remove(10);
+	btree.remove(420);
 	btree.insert(20);
 	btree.insert(20);
 	btree.insert(30);
@@ -267,13 +269,18 @@ int main() {
 	btree.insert(12);
 	btree.insert(15);
 	btree.insert(14);
+	btree.remove(20);
+	btree.remove(20);
+	btree.remove(12);
 	for (int i = 100; i < 200; i++) {
 		btree.insert(i);
 	}
 	btree.insert(69);
-	std::cout << btree.get(20);
+	std::cout << btree.get(20)<<'\n';
 	btree.remove(20);
-	std::cout << btree.get(20);
+	std::cout << btree.get(20)<<'\n';
+	btree.remove(20);
+
 
 	return 0;
 }
